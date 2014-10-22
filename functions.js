7a6a38425b2fa22573473d1f79ff608d1bc347b7
Michael Mayer <me@micjm.com>
2014-10-21 17:00:11 -0700

    fixed recursive diving

--- functions.js
+++ functions.js
@@ -24,6 +24,58 @@ theater_code_objects = [
 
 ]
 
+theater_detail = [
+
+	{"1657":{
+		"name":"Laurelhurst Theater",
+		"address":" 2735 E Burnside St, Portland, OR 97214",
+		"phone": "(503) 232-5511"
+		}
+	},
+	{"238":{
+		"name":"Bagdad Theater",
+		"address":"3702 SE Hawthorne Blvd, Portland, OR 97214",
+		"phone": "(503) 467-7521"
+		}
+	},
+	{"6734":{
+		"name":"Hollywood Theater",
+		"address":"4122 NE Sandy Blvd, Portland, OR 97212",
+		"phone": "(503) 281-4215"
+		}
+	},
+	{"1196":{
+		"name":"Kennedy School",
+		"address":"5736 NE 33rd Ave, Portland, OR 97211",
+		"phone": "(503) 249-3983"
+		}
+	},
+	{"243":{
+		"name":"Mission Theater",
+		"address":"1624 NW Glisan St, Portland, OR 97209",
+		"phone": "(503) 223-4527"
+		}
+	},
+	{"240":{
+		"name":"Cinema 21",
+		"address":"616 NW 21st Ave, Portland, OR 97209",
+		"phone": "(503) 223-4515"
+		}
+	},
+	{"3031":{
+		"name":"Living Room Theater",
+		"address":"341 SW 10th Ave, Portland, OR 97205",
+		"phone": "(971) 222-2010"
+		}
+	},
+	{"2213":{
+		"name":"Academy Theater",
+		"address":"7818 SE Stark St, Portland, OR 97215",
+		"phone": "(503) 252-0500"
+		}
+	}
+]
+
 //Function to parse date-time and return html element
 
 function time_to_span (date) {
@@ -55,41 +107,44 @@ function mpaa_check(rate) {
 
 function showtimes_by_theater(data, targetDiv) {
 	//loop through theaters
-	$.each(theater_code_objects, function(index, obj) {
+	$.each(theater_detail, function(index, obj) {
 		//loop through movie title objects
-		var code = Object.keys(obj)[0]
-		var location = obj[code]
+		var code = Object.keys(obj)
+		theater_obj = obj[Object.keys(obj)]
+		var location = theater_obj.name
+		var address = theater_obj.address
 		var content = "<div class='main_container'>"
 		var content = content + "<h1 class='main_title'>"+location+"</h1>"
+		var content = content + "<div class='main_info'>"+address+"</div>"
 		var titles_used = []
+		content = content + "<div class='secondary'>"
 
 		//loop through theater data
 		$.each(data, function(index, val) {
-			content = content + "<div class='secondary'>"
+			
 			showtime_array = val.showtimes
 			//loop through showtime arrays
 			$.each(showtime_array, function(i, v) {
 				var theatre_id = v.theatre.id
 				//check if theater_id matches current container
-				if (theatre_id === code) {
+				if (theatre_id == code) {
 					var title = val.title
 					var time = v.dateTime
 					console.log(time)
-					
 					if (titles_used.indexOf(title) === -1) {
-						content = content + "<h2 class='secondary_title'>"+val.title+"</h2>"
+						//content = content + "<div class='secondary'>"
+						content = content + "<h2 class='secondary_name'>"+val.title+"</h2>"
 						content = content + time_to_span(time)
 					} else {
 						content = content + time_to_span(time)
 					}
-					
 					titles_used.push(title)
-					
 				}
+				//content = content + "</div>" //close secondary div
 			})
-		content = content + "</div>" //close secondary div
+		
 		})
-
+		var content = content + "</div>" //cloase secondary
 		var content = content + "</div>" //cloase main_container
 		targetDiv.append(content)
 	})
